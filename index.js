c14f46d7d6c043fc5b08704b7c400018d8ee8c0f
Alexandre R. <sadnessnc@gmail.com>
2025-03-25 10:57:10 +0100

    Upload Files

---
+++ index.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+console.log(
+    chalk.yellow("Kevin Tran!"));
+
+console.log(
+    chalk.green("Lutin Malicieux!"));
+
+console.log(
+    chalk.red("Dylan Thiry!"));
+
+console.log(
+    chalk.magenta("Alexandre Astier!"));
